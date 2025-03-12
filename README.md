ba0ba445167e6a8ad1e7567568f488aa49aedb6a
Falcon Echelon <echelonfalcon1@gmail.com>
2025-03-12 16:05:41 +0000

    Current Version of MyCV

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # MyCV_Website
 
 I created this webiste as a different way of showing my CV and to test myself for design and coding.
-# MyCV
-# MyCV
---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+const accordionItems = document.querySelectorAll('.accordion-item');
+
+accordionItems.forEach(item => {
+  item.addEventListener('click', () => {
+    item.classList.toggle('active');
+  });
+});
+
